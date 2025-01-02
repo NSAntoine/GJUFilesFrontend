dedfaba07394f46c882321179d1735fa44c9b5c8
NSAntoine (Osama Alhour) <osama.id78@gmail.com>
2025-01-02 13:40:24 +0300

    Remove strict mode for testing

--- src/main.tsx
+++ src/main.tsx
@@ -23,8 +23,8 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  // </StrictMode>,
 )
 
